a3b927ca6b0ad31256c84800e7d9fd1924f5bbe7
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-09-01 19:06:16 -0300

    Listando e cadastrando corretamente

--- src/controllers/HomePageController.js
+++ src/controllers/HomePageController.js
@@ -1,10 +1,25 @@
 const isAuthenticated = require('./midlewareController');
+const funcionarioModel = require('../models/funcionarioModel'); // Importe o módulo aqui
 
 module.exports = {
   HomePage: [
     isAuthenticated, // Use o middleware aqui
-    (req, res) => {
-      res.render('HomePage', { pageTitle: 'HomePage' });
+    async (req, res) => {
+      try {
+        // Supondo que você tenha uma função para buscar os funcionários no modelo
+        const usuarioId = req.session?.user?.idusuario;
+        const resultado = await funcionarioModel.listarFuncionarioPorUsuario(usuarioId);
+
+        if (resultado.success) {
+          const funcionarios = resultado.funcionarios;
+          res.render('HomePage', { pageTitle: 'HomePage', success: true, funcionarios });
+        } else {
+          res.render('HomePage', { pageTitle: 'HomePage', success: false });
+        }
+      } catch (error) {
+        console.error('Erro ao carregar funcionários:', error);
+        res.render('HomePage', { pageTitle: 'HomePage', success: false });
+      }
     }
   ]
 };
