1eefcd0560ac87c51e149118bf91938133eac9e6
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-09-07 18:59:04 -0300

    Ajustes dos cards

--- controllers/cadastroController.js
+++ controllers/cadastroController.js
@@ -2,20 +2,23 @@ const usuarioModel = require('../models/usuarioModel');
 
 class CadastroController {
     async showCadastroForm(req, res) {
-        res.render('cadastro');
+        res.render('login', { successMessage: req.flash('successMessage') });
     }
 
     async cadastrarEmpresa(req, res, next) {
-        const {nome, email, senha} = req.body;
+        const { nome, email, senha } = req.body;
 
         try {
             const emailExists = await usuarioModel.checkEmailExists(email);
             if (emailExists) {
-                return res.status(400).send('Email já cadastrado');
+                req.flash('errorMessage', 'Este email já está cadastrado.');
+                res.redirect('/register');
+                return;
             }
 
             await usuarioModel.inserirUsuario(nome, email, senha);
             console.log('Empresa cadastrada com sucesso');
+            req.flash('successMessage', 'Conta criada com sucesso.');
             res.redirect('/user');
         } catch (err) {
             next(err);
--- routers/cadastroRouter.js
+++ routers/cadastroRouter.js
@@ -3,7 +3,7 @@ const cadastroRouter = express.Router();
 const cadastroController = require('../controllers/cadastroController');
 
 // Rota para exibir o formulário de cadastro de empresa
-cadastroRouter.get('/user/register', cadastroController.showCadastroForm);
+cadastroRouter.get('/register', cadastroController.showCadastroForm);
 
 // Rota para cadastrar uma nova empresa
 cadastroRouter.post('/user/register', cadastroController.cadastrarEmpresa);
