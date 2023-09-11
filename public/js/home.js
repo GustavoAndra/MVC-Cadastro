f672ec0dc8e7b76bd1961b2fc027760814cb5249
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-09-11 11:32:53 -0300

    Ajuste na navegação

--- public/js/home.js
+++ public/js/home.js
@@ -27,3 +27,21 @@ window.addEventListener('resize', function(event) {
         toggleSidebar();
     }
 });
+
+window.addEventListener('scroll', function() {
+    var scrollToTop = document.getElementById('scrollToTop');
+    if (window.scrollY > 100) {
+        scrollToTop.classList.remove('hidden');
+    } else {
+        scrollToTop.classList.add('hidden');
+    }
+});
+
+// Role suavemente para o topo quando a seta for clicada
+document.getElementById('scrollToTopButton').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
